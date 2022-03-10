84be1c97603c2d3dd7c9d17fa1c9522f27e7f4ef
Sander <info@sanderwooning.nl>
2022-03-10 18:24:47 +0100

    Can_Player_Win_functie

--- test_connect.py
+++ test_connect.py
@@ -75,6 +75,15 @@ class TestCF(unittest.TestCase):
         self.assertTrue(cf.is_horizontal_win(BLUE))
         self.assertFalse(cf.is_horizontal_win(RED))
 
+    def test_is_horizontal_long(self):
+        cf = ConnectFour(10,4)
+        cf.board = np.array([[0,0,0,0,0,0,0,0,0,0,0,0],
+                             [0,0,0,0,0,0,0,0,0,0,0,0],
+                             [0,0,0,0,0,0,0,0,0,0,0,0],
+                             [RED,RED,BLUE,BLUE,RED,RED,RED,RED,BLUE,BLUE]
+                             ])
+        self.assertTrue(cf.is_horizontal_win(RED))
+        self.assertFalse(cf.is_horizontal_win(BLUE))
 
     def test_is_vertical_win(self):
         cf = ConnectFour(4,4)
@@ -85,6 +94,8 @@ class TestCF(unittest.TestCase):
 
         self.assertTrue(cf.is_vertical_win(RED))
         self.assertFalse(cf.is_vertical_win(BLUE))
+
+
 
 
     def test_is_diagonal_win(self):
@@ -179,6 +190,16 @@ class TestCF(unittest.TestCase):
 
         self.assertFalse(cf.can_player_win(BLUE)) 
         self.assertTrue(cf.can_player_win(RED))
+
+    def test_can_player_win_empty_board(self):
+        cf = ConnectFour(4,4)
+        cf.board=np.array([[0, 0, 0, 0],
+                           [0, 0, 0, 0],
+                           [0, 0, 0, 0],
+                           [0, 0, 0, 0]])
+        self.assertFalse(cf.can_player_win(BLUE))
+        self.assertTrue(cf.can_player_win(RED))
+
 
     def test_best_move_greedy(self):
         cf = ConnectFour(4,4)
