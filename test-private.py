36cb7d272337f62bae3a73bcb947a573a71c0c04
Sander Wooning <s.wooning@newcompliance.com>
2022-03-16 20:12:22 +0100

    Changes

--- test-private.py
+++ test-private.py
@@ -8,60 +8,76 @@ from connect import RED, BLUE, RED_WIN, BLUE_WIN, DRAW, IN_PROGRESS
 
 class PrivateTestCF(unittest.TestCase):
 
+    def test_no_avail_moves(self):
+        cf = ConnectFour(4, 4)
+        cf.board = np.array([[RED,RED,RED,RED],
+                             [RED,RED,RED,RED],
+                             [RED,RED,RED,RED],
+                             [RED,RED,RED,RED],
+                             ])
+        self.assertTrue(len(cf.get_available_moves()) == 0 )
+
     def test_win_asymatric_board(self):
-        cf = ConnectFour(6,4)
-        cf.board = np.array([[0,0,0,0,RED,BLUE],
-                             [0,0,0,RED,0,BLUE],
-                             [0,0,RED,0,0,RED],
-                             [0,RED,BLUE,BLUE,BLUE,BLUE]
-        ])
+        cf = ConnectFour(6, 4)
+        cf.board = np.array([[0, 0, 0, 0, RED, BLUE],
+                             [0, 0, 0, RED, 0, BLUE],
+                             [0, 0, RED, 0, 0, RED],
+                             [0, RED, BLUE, BLUE, BLUE, BLUE]
+                             ])
         self.assertTrue(cf.has_player_won(RED))
         self.assertTrue(cf.has_player_won(BLUE))
 
-        cf = ConnectFour(4,6)
-        cf.board = np.array([[RED,0,0,BLUE],
-                             [0,RED,BLUE,0],
-                             [0,BLUE,RED,0],
-                             [BLUE,0,0,RED],
-                             [0,0,0,0],
-                             [0,0,0,0]
+        cf = ConnectFour(4, 7)
+        cf.board = np.array([[RED, 0, 0, BLUE],
+                             [0, RED, RED, 0],
+                             [0, BLUE, RED, 0],
+                             [BLUE, 0, 0, RED],
+                             [0, BLUE, 0, 0],
+                             [0, 0, BLUE, 0],
+                             [0, 0, 0, BLUE]
                              ])
 
         self.assertTrue(cf.has_player_won(RED))
         self.assertTrue(cf.has_player_won(BLUE))
 
-        cf = ConnectFour(10,4)
-        cf.board = np.array([[0,0,0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,RED,RED,RED,RED],
-                             [RED,RED,BLUE,BLUE,RED,BLUE,RED,RED,BLUE,BLUE]
+        cf = ConnectFour(10, 4)
+        cf.board = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0, RED, RED, RED, RED],
+                             [RED, RED, BLUE, BLUE, RED, BLUE, RED, RED, BLUE, BLUE]
                              ])
         self.assertTrue(cf.is_horizontal_win(RED))
         self.assertFalse(cf.is_horizontal_win(BLUE))
 
     def test_large_diagonal_check(self):
-
         """
-        A testcase for all possible win situations on a large board.
+        A testcase for large diagonal wins. . Floating doesn't affect test-case.
         :return:
         """
 
         cf = ConnectFour(6, 6)
-        cf.board = np.array([[0, 0, 0, 0, 0, BLUE],
-                             [0, 0, 0, BLUE, RED, RED],
-                             [RED, BLUE, BLUE, RED, BLUE, RED],
-                             [BLUE, BLUE, RED, RED, RED, RED],
-                             [BLUE, RED, RED, BLUE, BLUE, RED],
-                             [BLUE, BLUE, RED, RED, BLUE, BLUE]
+        cf.board = np.array([[0, 0, 0, 0, 0, 0],
+                             [0, 0, 0, 0, RED, 0],
+                             [0, 0, 0, RED, 0, 0],
+                             [0, 0, RED, 0, 0, 0],
+                             [0, RED, 0, 0, 0, 0],
+                             [0, 0, 0, 0, 0, 0]
                              ])
+
         self.assertTrue(cf.is_diagonal_win(RED))
-        self.assertTrue(cf.is_vertical_win(RED))
-        self.assertTrue(cf.is_horizontal_win(RED))
+        self.assertFalse(cf.is_diagonal_win(BLUE))
+
+        cf = ConnectFour(6, 6)
+        cf.board = np.array([[0, 0, 0, 0, 0, 0],
+                             [0, BLUE, 0, 0, 0, 0],
+                             [0, 0, BLUE, 0, 0, 0],
+                             [0, 0, 0, BLUE, 0, 0],
+                             [0, 0, 0, 0, BLUE, 0],
+                             [0, 0, 0, 0, 0, 0],
+                             ])
+
         self.assertTrue(cf.is_diagonal_win(BLUE))
-        self.assertFalse(cf.is_vertical_win(BLUE))
-        self.assertFalse(cf.is_horizontal_win(BLUE))
-
-
+        self.assertFalse(cf.is_diagonal_win(RED))
 
     # def test_can_player_win_empty_board(self):
     #     cf = ConnectFour(4,5)
@@ -84,13 +100,12 @@ class PrivateTestCF(unittest.TestCase):
     #     self.assertTrue(cf.can_player_win(BLUE))
     #     self.assertTrue(cf.can_player_win(RED))
 
-    def test_can_player_win_empty_board_45(self):
-        cf = ConnectFour(5,5)
-        cf.board=np.array([[0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0]])
-        self.assertTrue(cf.can_player_win(BLUE))
-        self.assertTrue(cf.can_player_win(RED))
-
+    # def test_can_player_win_empty_board_45(self):
+    #     cf = ConnectFour(5,5)
+    #     cf.board=np.array([[0, 0, 0, 0, 0],
+    #                        [0, 0, 0, 0, 0],
+    #                        [0, 0, 0, 0, 0],
+    #                        [0, 0, 0, 0, 0],
+    #                        [0, 0, 0, 0, 0]])
+    #     self.assertTrue(cf.can_player_win(BLUE))
+    #     self.assertTrue(cf.can_player_win(RED))
